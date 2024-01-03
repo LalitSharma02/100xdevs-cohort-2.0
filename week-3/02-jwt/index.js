b401835c9cf54450369b00544c6602895aa50180
Lalit Sharma <lalit.sharma6289@gmail.com>
2024-01-03 18:07:03 +0530

    Updated Solutions

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const zod = require("zod");
 
 
 /**
@@ -13,8 +14,22 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+const emailSchema = zod.string().email();
+const paswordSchema = zod.string().min(6);
+
 function signJwt(username, password) {
     // Your code here
+    const emailResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+
+    if(!emailResponse.success || !passwordResponse.success){
+        return null;
+    }
+    const signature = jwt.sign({
+        username} , jwtPassword);
+
+    return signature;
+
 }
 
 /**
@@ -27,6 +42,14 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    let ans = true;
+    try{
+        jwt.verify(token,jwtPassword);
+    }catch(e){
+        ans = false;
+    }
+    return ans;
+
 }
 
 /**
@@ -38,6 +61,13 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const decoded = jwt.decode(token);
+    if(decoded){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
